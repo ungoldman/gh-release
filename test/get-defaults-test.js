59992a454baff3b4bd702f8859c5d63b2713474f
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2023-01-20 15:36:49 -0800

    fix: rm rimraf, use fs.rm

--- test/get-defaults-test.js
+++ test/get-defaults-test.js
@@ -1,11 +1,11 @@
 const test = require('tape')
+const fs = require('fs')
 const path = require('path')
 const tmp = require('tmp')
 const gitPullOrClone = require('git-pull-or-clone')
-const rimraf = require('rimraf')
-
 const getDefaults = require('../bin/lib/get-defaults')
-const remote = 'https://github.com/bcomnes/gh-release-test.git'
+
+const GH_RELEASE_TEST_REPO = 'https://github.com/bcomnes/gh-release-test.git'
 
 let repoDir, tmpDir
 
@@ -14,7 +14,7 @@ test('Set up test environment', function (t) {
   tmpDir = tmp.dirSync({ unsafeCleanup: true })
   repoDir = path.join(tmpDir.name, 'test-repo')
   t.ok(tmpDir.name, 'valid tmp dir exists')
-  gitPullOrClone(remote, repoDir, function (err) {
+  gitPullOrClone(GH_RELEASE_TEST_REPO, repoDir, function (err) {
     t.error(err, 'cloned test repo')
   })
 })
@@ -73,7 +73,7 @@ test('get-defaults errors out with an invalid repository URL', function (t) {
 
 test('Clean up tests', function (t) {
   t.plan(1)
-  rimraf(tmpDir.name, function (err) {
+  fs.rm(tmpDir.name, { recursive: true, force: true }, function (err) {
     t.error(err, 'repo cleaned up')
   })
 })
